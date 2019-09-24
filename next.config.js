27f2a8fa8dc1e14ad6a959abff9566ce1ff00998
Nick Tennies <tenniesdesign@gmail.com>
2019-09-24 15:25:16 -0400

    updates next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const withCSS = require('@zeit/next-css');
 const webpack = require('webpack');
 
-const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
+const apiKey = process.env.SHOPIFY_API_KEY;
 
 module.exports = withCSS({
   webpack: (config) => {
