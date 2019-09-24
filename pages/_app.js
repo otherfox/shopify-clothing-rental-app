d79f7223e274c45ce275759fd9c3dbe388716f1e
Nick Tennies <tenniesdesign@gmail.com>
2019-09-24 13:33:46 -0400

    adds link and fetch to ApolloClient

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
+import { createHttpLink } from 'apollo-link-http';
+import fetch from 'node-fetch';
 import '@shopify/polaris/styles.css';
 import Cookies from 'js-cookie';
 import ApolloClient from 'apollo-boost';
@@ -11,11 +13,16 @@ const client = new ApolloClient({
   fetchOptions: {
     credentials: 'include'
   },
+  link: createHttpLink({
+    uri: '/graphql',
+    fetch: fetch
+  })
 });
 
 
 class MyApp extends App {
   render() {
+    console.log(client);
     const { Component, pageProps } = this.props;
     const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
     return (
